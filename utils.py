dfddf4466e702646b0f52fde4cdc002c14cea9a1
carlosdecarli <carloslorenzomdc@gmail.com>
2026-08-26 07:58:10 -0300

    handout corrigido

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def load_data(filename):
     with open("static/data/" + filename) as file:
@@ -8,3 +9,10 @@ def load_data(filename):
 def load_template(file):
     with open('static/templates/'+file) as file:
         return file.read()
+
+def add_note(titulo, detalhes):
+    filepath = os.path.join('static', 'data', 'notes.json')
+    notes = load_data('notes.json')
+    notes.append({'titulo': titulo, 'detalhes': detalhes})
+    with open(filepath, 'w', encoding='utf-8') as file:
+        json.dump(notes, file, ensure_ascii=False, indent=2)
